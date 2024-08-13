e8d4cb17612dff6ab1c445d6575815c5e2358e63
timothyngugi26 <timothyngugi26@gmail.com>
2024-08-13 13:05:05 -0700

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    A python script that returns the top 10 post
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ Function that queries the Reddit API and prints the titles of the first
+        10 hot posts listed for a given subreddit.
+
+    Keyword arguments:
+    subreddit -- a string representing the subreddit name
+    """
+    if not subreddit or not isinstance(subreddit, str):
+        print("None")
+        return
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {"User-Agent": "MyRedditBot/1.0"}
+
+    try:
+        response = requests.get(url, headers=headers, params={"limit": 10})
+        if response.status_code == 200:
+            data = response.json()
+            children = data["data"]["children"]
+            for child in children:
+                print(child["data"]["title"])
+        else:
+            print("None")
+    except Exception as e:
+        print("None")
+        return
