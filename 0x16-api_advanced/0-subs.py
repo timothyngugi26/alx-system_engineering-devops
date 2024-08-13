3e4fa933715c14d30dd58437ac75faee9f694d68
timothyngugi26 <timothyngugi26@gmail.com>
2024-08-13 12:16:51 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    if not subreddit or not isinstance(subreddit, str):
-        print("None")
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditBot/1.0"}
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    response = requests.get(url, headers=headers)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data["data"]["subscribers"]
-            return subscribers
-        else:
-            return 0
-    except Exception as e:
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
+
